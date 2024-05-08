0309342a9596dd95e5ec64776b167394863178c0
Ryan <rayanibrahim.benatallah@etu.usthb.dz>
2024-05-08 00:10:02 -0700

    [ALL_DONE]

--- answer.py
+++ answer.py
@@ -1,10 +1,12 @@
 
 
 def getAnswer(question, context, model):
-    template = "You are an industrial machines engineer specializing in injection molding machines, die casting machines, rubber injection machines, and high-speed packaging systems at Yizumi. \
-                Your role is to provide client support by answering inquiries, prioritizing the provided semantic search context. Use the context directly if it addresses the client's question comprehensively. \
-                Refer to your expertise only if the context is insufficient.\n\
-                Client Question: {question}\n\
+    template = "You are an expert in deep learning and computer vision\
+                Your role is to provide searchers with informations related to punlished research papers , \
+                prioritizing the provided semantic search context retrieved from the paper. \
+                Use the context directly if it addresses the researcher's question comprehensively. \
+                Refer to your expertise to augment the explanation only if you think the context is insufficient.\n\
+                Searcher Question: {question}\n\
                 Search Context: {context}"
 
     augmented_template = template.format(question=question, context=context)
